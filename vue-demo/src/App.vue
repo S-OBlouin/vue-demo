93991b39085449a1adc945a70c0ab78462638c7c
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-06-20 14:19:12 -0400

    API data
    Added the API call for the company names and added Axios

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div class=" bg-gray-400 ">
     <div class=" flex w-screen h-screen justify-center">
-      <form class="mt-40 h-52 w-56 flex-col flex bg-white shadow-xl items-center justify-center rounded">
-        <div>H3M Log In</div>
-        <select v-model="selected" class="mb-3 mt-3 px-1 w-52">
+      <form class="mt-40 h-72 w-80 flex-col flex bg-white shadow-xl items-center justify-center rounded">
+        <div class=" text-3xl">H3M Log In</div>
+        <select v-model="selected" class="mb-5 mt-5 px-1 w-52">
           <option disabled selected>Choose a bank</option>
-          <option v-for="company in companies">{{ company }}</option>
+          <option v-for="company in companies">{{ company.companyName }}</option>
         </select>
         <label>
           <input type="text" v-model="username" placeholder="Username"
-            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-3 rounded">
+            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-5 rounded">
         </label>
         <label>
           <input type="text" v-model="password" placeholder="Password"
-            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-3 rounded">
+            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-4 rounded">
         </label>
-        <button @click.prevent="logIn" class=" bg-green-300 rounded w-20 text-center">Log In</button>
+        <button @click.prevent="logIn" class=" bg-green-300 rounded w-20 text-center text-lg">Log In</button>
       </form>
     </div>
   </div>
@@ -23,13 +23,22 @@
 
 <script>
 import { ref } from 'vue';
+import axios from 'axios';
 export default {
   data () {
     return {
       selected: ref(''),
-      companies: ['Bank of ABC', 'Real Bank', 'CIBS'],
+      companies: [],
       username: ref(''),
       password: ref(''),
+    }
+  },
+  async mounted () {
+    try {
+      const response = await axios.get('http://213.159.5.243:8081/api/SubCompany/get-company-list');
+      this.companies = response.data
+    } catch (error) {
+      console.error(error);
     }
   },
   methods: {
