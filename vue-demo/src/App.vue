4bf497d6d57a760a0e57c24aa49ff9e8353e03cf
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-06-20 11:19:25 -0400

    Finalizing the login form

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class=" bg-gray-400 ">
-    <div class=" flex w-screen h-screen items-center justify-center">
-      <form class="mt-4 h-60 flex-row">
-        <select v-model="selected">
+    <div class=" flex w-screen h-screen justify-center">
+      <form class="mt-40 h-52 w-56 flex-col flex bg-white shadow-xl items-center justify-center">
+        <div>KROTON AML Log In</div>
+        <select v-model="selected" class="mb-3 mt-3 px-1 ">
           <option disabled selected>Choose a bank</option>
           <option v-for="company in companies">{{ company }}</option>
         </select>
         <label>
-          <input type="text" v-model="username" placeholder="Username">
+          <input type="text" v-model="username" placeholder="Username"
+            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-3 rounded">
         </label>
         <label>
-          <input type="text" v-model="password" placeholder="Password">
+          <input type="text" v-model="password" placeholder="Password"
+            class="block flex-1 border border-gray-500 py-1.5 pl-1 bg-white mb-3 rounded">
         </label>
-        <button @click="logIn">Log In</button>
+        <button @click.prevent="logIn" class=" bg-green-300 rounded w-20 text-center">Log In</button>
       </form>
     </div>
   </div>
@@ -24,13 +27,15 @@ export default {
   data () {
     return {
       selected: ref(''),
-      companies: [1, 2, 3],
+      companies: ['Bank of ABC', 'Real Bank', 'CIBS'],
       username: ref(''),
       password: ref(''),
     }
   },
-  logIn () {
-    console.log('allo')
+  methods: {
+    logIn () {
+      console.log(this.username)
+    }
   }
 }
 
