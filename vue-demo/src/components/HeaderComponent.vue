cd735b4fa4df415d4dd2ead2f098d85b4fb9bce6
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-06-27 13:23:59 -0400

    final commit of the day

--- vue-demo/src/components/HeaderComponent.vue
+++ vue-demo/src/components/HeaderComponent.vue
@@ -1,6 +1,9 @@
 <template>
     <div class=" bg-yellow-600 flex justify-between h-16">
-        <router-link to="/"><img src="@/assets/img/icon.png" alt="Home Icon" class=" h-14 ml-2" /></router-link>
+        <div class="flex justify-center items-center">
+            <router-link to="/"><img src="@/assets/img/icon.png" alt="Home Icon" class=" h-14 ml-2" /></router-link>
+            <p class="pl-4 font-bold text-xl">Kroton™ AML Platform</p>
+        </div>
         <div class=" flex flex-row-reverse justify-center items-center">
             <img src="@/assets/img/logout.svg" alt="Logout Icon" @click="logOut" class="cursor-pointer" />
             <p class=" pr-2 font-bold text-xl">{{ userStore.username }}</p>
