3ddcce1fc11672bc4599a4731bdb0f587e3befbe
Simon-Olivier Blouin <soblouin2244@gmail.com>
2023-07-20 12:57:25 -0400

    Start of SQL

--- vue-demo/src/services/MinerDataServices.js
+++ vue-demo/src/services/MinerDataServices.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 
-const url = 'http://213.159.5.243:8081/api/'
+const url = "http://213.159.5.243:8081/api/";
 
 class MinerDataServices {
-    async getMiners(data){
-        return await axios.get(url + 'MinerTraining/Miners', {headers: {'Authorization': `Bearer ${data}`}})
-    }
-    async getFeatures(data){
-        return await axios.get(url + 'MinerTraining/features', {headers: {'Authorization': `Bearer ${data}`}})
-    }
-    async getStatuses(data){
-        return await axios.get(url + 'alert/statuses', {headers: {'Authorization': `Bearer ${data}`}})
-    }
-    async getActivities(data){
-        return await axios.post(url + 'activity/run-activities', {headers: {'Authorization': `Bearer ${data.token}`}, body: {data}})
-    }
-    async getMinerList(data){
-        return await axios.get(url + 'activity/get-miner-list', {headers: {'Authorization': `Bearer ${data}`}})
-    }
-    
+  async getMiners(data) {
+    return await axios.get(url + "MinerTraining/Miners", { headers: { Authorization: `Bearer ${data}` } });
+  }
+  async getFeatures(data) {
+    return await axios.get(url + "MinerTraining/features", { headers: { Authorization: `Bearer ${data}` } });
+  }
+  async getStatuses(data) {
+    return await axios.get(url + "alert/statuses", { headers: { Authorization: `Bearer ${data}` } });
+  }
+  async getActivities(data, token) {
+    return await axios.post(url + "activity/run-activities", { headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` }, data: { data } });
+  }
+  async getMinerList(data) {
+    return await axios.get(url + "activity/get-miner-list", { headers: { Authorization: `Bearer ${data}` } });
+  }
 }
 
-export default new MinerDataServices
+export default new MinerDataServices();
--- vue-demo/src/views/TaskManagerView.vue
+++ vue-demo/src/views/TaskManagerView.vue
@@ -1,36 +1,42 @@
 <template>
-    <h1>hi</h1>
+    <button @click="activity" class="border bg-red-500">hi</button>
 </template>
 
 <script>
 import MinerDataServices from '@/services/MinerDataServices.js'
 import { useUserStore } from '@/stores/UserStore';
-export default{
-    name:'TaskManagerView',
-    setup(){
+export default {
+    name: 'TaskManagerView',
+    setup () {
         const store = useUserStore()
 
         return { store }
     },
-    data(){
+    data () {
         return {
             miners: [],
             activities: [],
         }
     },
-    async beforeMount(){
+    async beforeMount () {
         const res = await MinerDataServices.getMinerList(this.store.token)
         this.miners = JSON.parse(JSON.stringify(res.data))
+        console.log(this.miners)
     },
-    async mounted(){
-        const res = await MinerDataServices.getActivities({
-            page: 0,
-            miners: this.miners,
-            username: '',
-            date: null
-        })
-        this.activities = JSON.parse(JSON.stringify(res.data))
-        console.log(this.activities)
+    async mounted () {
+
     },
+    methods: {
+        async activity () {
+            const res = await MinerDataServices.getActivities(JSON.stringify({
+                page: 0,
+                miners: this.miners,
+                username: "",
+                date: null
+            }), this.store.token)
+            this.activities = JSON.parse(JSON.stringify(res.data))
+            console.log(this.activities)
+        }
+    }
 }
 </script>
